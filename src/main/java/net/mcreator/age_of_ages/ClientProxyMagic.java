a42865bcb4a42f4d293a2eae0f1904d66f98fcc4
gaber <gaber@DESKTOP-I4OUS4V.lan>
2020-09-09 21:03:40 +0300

    thing

---
+++ src/main/java/net/mcreator/age_of_ages/ClientProxyMagic.java
@@ -0,0 +1,26 @@
+package net.mcreator.age_of_ages;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyMagic implements IProxyMagic {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("magic");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
